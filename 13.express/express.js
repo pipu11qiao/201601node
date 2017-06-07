2cc867e2db39780b12160e203aaab93663ac890b
pipu11qiao <18854558196@163.com>
2017-06-07 16:27:08 +0800

    上传

--- 13.express/express.js
+++ 13.express/express.js
@@ -1,1 +1,17 @@
 //-->> Created by pipu on 2017/6/7.
+
+// 加载 express
+let express = require('express');
+// 获取配置对象
+let app = express();
+// 配置路由
+// 当用户访问/的时候，会执行后面的回调函数
+app.get('/',function (req,res) {
+	// send 可以自动判断参数类型，自动转换相应信息
+	// 并且自动设置Content-Type
+	res.send('hello world');
+});
+// 启动服务器
+app.listen(3000,function () {
+	console.log('server is listening port 3000')
+});
